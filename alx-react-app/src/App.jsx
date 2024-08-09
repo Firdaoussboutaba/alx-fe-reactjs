96fe83ef29237db5330be38b43d4a752440ad1bb
firdaouss0 <firdaouss.boutaba@gmail.com>
2024-08-09 16:45:33 +0100

    add userprofile to app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,6 +8,7 @@ import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 
 
@@ -28,6 +29,12 @@ function App() {
             <MainContent />
             <Footer />
         </div> 
+        <div>
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        </div>
+
+
+
 
 
       
