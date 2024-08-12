3f13d0799c6c331a28823e366ee23e8ab6f5720b
firdaouss0 <firdaouss.boutaba@gmail.com>
2024-08-12 12:27:53 +0100

    duplicate directory and add inline css to header footer maincontent userprofile

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+    return (
+        <main style={{ backgroundColor: '#f0f0f0', padding: '20px', margin: '20px' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
