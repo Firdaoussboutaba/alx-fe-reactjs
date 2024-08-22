279132ce3ca99f0a85f97b99c0dd30e62c7388d8
firdaouss0 <firdaouss.boutaba@gmail.com>
2024-08-22 16:43:31 +0100

    add recipeStore.js to src/compoents

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,8 +2,10 @@ import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe]
+  })),
   setRecipes: (recipes) => set({ recipes }),
 }));
 
-export default useRecipeStore;
+export { useRecipeStore };
