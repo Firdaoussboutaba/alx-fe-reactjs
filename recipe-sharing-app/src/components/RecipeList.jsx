1a512c1fbdd4224498bb644d6c0a45b53d992fa9
firdaouss0 <firdaouss.boutaba@gmail.com>
2024-08-22 23:29:46 +0100

    modification in recipelist.jsx file

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,21 @@
 import { useRecipeStore } from './recipeStore';
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
   return (
 
     <div>
-    {filteredRecipes.map(recipe => (
-      <div key={recipe.id}>
-        <h3>{recipe.title}</h3>
-        <p>{recipe.description}</p>
-      </div>
-    ))}
-  </div>
+      {filteredRecipes.map(recipe => (
+        <div key={recipe.id}>
+          <Link to={`/recipe/${recipe.id}`}>
+            <h3>{recipe.title}</h3>
+          </Link>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
 
 
     
