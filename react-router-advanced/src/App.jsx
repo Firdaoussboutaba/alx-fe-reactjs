ebe9a837692d1f6e2b493cd0cd3a66389be95591
firdaouss0 <firdaouss.boutaba@gmail.com>
2024-08-30 01:08:52 +0100

    useAuth

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -5,8 +5,8 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
-import NotFound from './components/NotFound';
 import BlogPost from './components/BlogPost';
+import NotFound from './components/NotFound';
 import ProtectedRoute from './components/ProtectedRoute';
 
 
--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
 // src/components/ProtectedRoute.jsx
 import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth'; // Import the custom hook
 
 const ProtectedRoute = ({ element, ...rest }) => {
-  const isAuthenticated = false; // Simulate authentication check
+  const { isAuthenticated } = useAuth();
 
   return (
     <Route
