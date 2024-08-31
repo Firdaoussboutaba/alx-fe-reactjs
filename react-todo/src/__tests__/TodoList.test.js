716cdb43f0911d5c562e556cec4d64fc6d1c915b
firdaouss0 <firdaouss.boutaba@gmail.com>
2024-08-31 14:10:18 +0100

    Todo List Component in React

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import TodoList from '../components/TodoList';
+
+test('renders TodoList component', () => {
+    render(<TodoList />);
+    expect(screen.getByText('Todo List')).toBeInTheDocument();
+    expect(screen.getByText('Learn React')).toBeInTheDocument();
+    expect(screen.getByText('Build a Todo App')).toBeInTheDocument();
+});
