3f6fc94a874602a5ac65decccde4c3ed2e9d800b
firdaouss0 <firdaouss.boutaba@gmail.com>
2024-09-06 12:05:09 +0100

    style useprofile with tailwindcss

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+  <div>
+      <UserProfile />
+    </div>
+
       <div className="p-6 bg-blue-500 text-white">
       Hello, Tailwind CSS with React!
     </div>
