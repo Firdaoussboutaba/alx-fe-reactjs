4e02085f84f62e7e33d775921b3f0eacd6b6f4d5
firdaouss0 <firdaouss.boutaba@gmail.com>
2024-09-14 00:07:30 +0100

    build recipe detail page

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<div className="text-blue-500">
-      <h1>Hello, Tailwind CSS!</h1>
-    </div>
+  <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+
+
+
+
 
       <div>
         <a href="https://vitejs.dev" target="_blank">
